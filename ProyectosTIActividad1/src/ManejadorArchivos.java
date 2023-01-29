a396418d042b8c7da4503a83d7c11fdb1a964403
Patricio <patoestradac@gmail.com>
2023-01-29 02:29:00 -0500

    Comentarios añadidos al funcionamiento del programa

--- ProyectosTIActividad1/src/ManejadorArchivos.java
+++ ProyectosTIActividad1/src/ManejadorArchivos.java
@@ -2,15 +2,16 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-public class Archivo
+public class ManejadorArchivos      //Clase para manejar los archivos de HTML
 {
+    //Abre con el buscador predeterminado la página con el link que recibe de parametro
     public void abrirPagina(String link) throws IOException
     {
         File file = new File (link);
         Desktop.getDesktop().browse(file.toURI());
     }
 
-    public String numeroAString(int numero)
+    public String numeroAString(int numero)     //Se usa para crear la String que cambie el numero de la iteración por el nombre real de la página de HTML
     {
         String regresar="";
 
