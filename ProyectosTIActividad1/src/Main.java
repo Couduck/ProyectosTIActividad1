07acd49cf0884c5f5b4237d9269eaf6b1f35cdff
Patricio <patoestradac@gmail.com>
2023-01-30 00:37:24 -0500

    Avance Actividad 2 creado

--- ProyectosTIActividad1/src/Main.java
+++ ProyectosTIActividad1/src/Main.java
@@ -5,65 +5,8 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
-        //Empieza el conteo de los milisegundos que pasan en el programa completo
-        long inicioPrograma = System.currentTimeMillis();
-
-        FileWriter salida = new FileWriter("Salida.txt");       //Se crea el archivo de salida de la información
-
         ManejadorArchivos manejadorArchivos = new ManejadorArchivos();      //Se crea objeto de tipo manejadorArchivos
-
-        boolean primeraVez = true;      //Variable booleana que indica si se realizará la primera escritura en el archivo de salida
-
-        long inicioCargaPaginas = System.currentTimeMillis();   //Empieza el conteo que indica el tiempo que el sistema tarda en cargar todas las páginas
-
-        //Desde el archivo 002 hasta el 503 realiza este ciclo
-        for(int i = 2; i < 504; i++)
-        {
-            //Se genera el link del archivo a leer en la iteración
-            String linkActual = "Files\\" + manejadorArchivos.numeroAString(i) + ".html";
-
-            //Empieza el conteo del tiempo que se tarda en abrir la pagina, se realiza la apertura y se termina el conteo
-            long inicioLeerPagina = System.currentTimeMillis();
-            manejadorArchivos.abrirPagina(linkActual);
-            long finLeerPagina = System.currentTimeMillis();
-
-            //Se obtiene el tiempo total de duración del conteo
-            long duracionLeerPagina = finLeerPagina - inicioLeerPagina;
-
-            //Se añade al archivo de salida el nombre de la página junto con su duración en segundos
-            String entradaPagina = linkActual + "\t" + duracionLeerPagina/1000.0;
-
-            //Dependiendo de si es la primera escritura, se escrbe o se anexa en el documento de texto
-            if(primeraVez)
-            {
-                salida.write(entradaPagina + "\n");
-                primeraVez = false;
-            }
-
-            else
-            {
-                salida.append(entradaPagina + "\n");
-            }
-
-        }
-
-        //Termina el conteo de la carga de páginas
-        long finCargaPaginas = System.currentTimeMillis();
-
-        //Se obtiene la duración total del conteo
-        long duracionCargaPaginas = finCargaPaginas - inicioCargaPaginas;
-
-        //Se añade el tiempo al documento de salida
-        salida.append("\nEl tiempo requerido para cargar todas las paginas fue de fue de " + duracionCargaPaginas/1000.0 + " segundos");
-
-        //Mismo proceso de arriba pero con la duración total del programa
-        long finPrograma = System.currentTimeMillis();
-
-        long duracionPrograma = finPrograma - inicioPrograma;
-
-        salida.append("\nLa duración del programa fue de " + duracionPrograma/1000.0 + " segundos");
-
-        //Se cierra el documento
-        salida.close();
+        //manejadorArchivos.abrirTodaslasPaginas();
+        manejadorArchivos.removerTodasLasEtiquetas();
     }
 }
--- ProyectosTIActividad1/src/ManejadorArchivos.java
+++ ProyectosTIActividad1/src/ManejadorArchivos.java
@@ -1,9 +1,152 @@
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class ManejadorArchivos      //Clase para manejar los archivos de HTML
 {
+    public void removerTodasLasEtiquetas() throws IOException
+    {
+        long inicioPrograma = System.currentTimeMillis();
+
+        FileWriter salida = new FileWriter("Salidas\\Salida2.txt");       //Se crea el archivo de salida de la información
+
+        boolean primeraVez = true;      //Variable booleana que indica si se realizará la primera escritura en el archivo de salida
+
+        long inicioBorradoEtiquetas = System.currentTimeMillis();   //Empieza el conteo que indica el tiempo que el sistema tarda en cargar todas las páginas
+
+        //Desde el archivo 002 hasta el 503 realiza este ciclo
+        for(int i = 2; i < 504; i++)
+        {
+            //Se genera el link del archivo cuyas etiquetas sevan a remover en la iteración
+            String linkActual = "Files\\" + this.numeroAString(i) + ".html";
+
+            //Empieza el conteo del tiempo que se tarda en remover las etiquetas, se hace, se genera el archivo puro y se termina el conteo
+            long inicioLeerPagina = System.currentTimeMillis();
+            this.quitarEtiquetas(linkActual,i);
+            long finLeerPagina = System.currentTimeMillis();
+
+            //Se obtiene el tiempo total de duración del conteo
+            long duracionLeerPagina = finLeerPagina - inicioLeerPagina;
+
+            //Se añade al archivo de salida el nombre de la página junto con su duración en segundos
+            String entradaPagina = linkActual + "\t" + duracionLeerPagina/1000.0;
+
+            //Dependiendo de si es la primera escritura, se escrbe o se anexa en el documento de texto
+            if(primeraVez)
+            {
+                salida.write(entradaPagina + "\n");
+                primeraVez = false;
+            }
+
+            else
+            {
+                salida.append(entradaPagina + "\n");
+            }
+        }
+
+        //Termina el proceso de borrado de etiquetas
+        long finBorradoEtiquetas = System.currentTimeMillis();
+
+        //Se obtiene la duración total del borrado de todas las etiquetas
+        long duracionBorradoEtiqutas = finBorradoEtiquetas - inicioBorradoEtiquetas;
+
+        //Se añade el tiempo al documento de salida
+        salida.append("\nEl tiempo requerido para retirar todas las etiquetas de todas las paginas fue de " + duracionBorradoEtiqutas/1000.0 + " segundos");
+
+        //Mismo proceso de arriba pero con la duración total del programa
+        long finPrograma = System.currentTimeMillis();
+
+        long duracionPrograma = finPrograma - inicioPrograma;
+
+        salida.append("\nLa duración del programa fue de " + duracionPrograma/1000.0 + " segundos");
+
+        //Se cierra el documento
+        salida.close();
+    }
+
+    public void abrirTodaslasPaginas() throws IOException
+    {
+        //Empieza el conteo de los milisegundos que pasan en el programa completo
+        long inicioPrograma = System.currentTimeMillis();
+
+        FileWriter salida = new FileWriter("Salidas\\Salida1.txt");       //Se crea el archivo de salida de la información
+
+        boolean primeraVez = true;      //Variable booleana que indica si se realizará la primera escritura en el archivo de salida
+
+        long inicioCargaPaginas = System.currentTimeMillis();   //Empieza el conteo que indica el tiempo que el sistema tarda en cargar todas las páginas
+
+        //Desde el archivo 002 hasta el 503 realiza este ciclo
+        for(int i = 2; i < 504; i++)
+        {
+            //Se genera el link del archivo a leer en la iteración
+            String linkActual = "Files\\" + this.numeroAString(i) + ".html";
+
+            //Empieza el conteo del tiempo que se tarda en abrir la pagina, se realiza la apertura y se termina el conteo
+            long inicioLeerPagina = System.currentTimeMillis();
+            this.abrirPagina(linkActual);
+            long finLeerPagina = System.currentTimeMillis();
+
+            //Se obtiene el tiempo total de duración del conteo
+            long duracionLeerPagina = finLeerPagina - inicioLeerPagina;
+
+            //Se añade al archivo de salida el nombre de la página junto con su duración en segundos
+            String entradaPagina = linkActual + "\t" + duracionLeerPagina/1000.0;
+
+            //Dependiendo de si es la primera escritura, se escrbe o se anexa en el documento de texto
+            if(primeraVez)
+            {
+                salida.write(entradaPagina + "\n");
+                primeraVez = false;
+            }
+
+            else
+            {
+                salida.append(entradaPagina + "\n");
+            }
+
+        }
+
+        //Termina el conteo de la carga de páginas
+        long finCargaPaginas = System.currentTimeMillis();
+
+        //Se obtiene la duración total del conteo
+        long duracionCargaPaginas = finCargaPaginas - inicioCargaPaginas;
+
+        //Se añade el tiempo al documento de salida
+        salida.append("\nEl tiempo requerido para cargar todas las paginas fue de fue de " + duracionCargaPaginas/1000.0 + " segundos");
+
+        //Mismo proceso de arriba pero con la duración total del programa
+        long finPrograma = System.currentTimeMillis();
+
+        long duracionPrograma = finPrograma - inicioPrograma;
+
+        salida.append("\nLa duración del programa fue de " + duracionPrograma/1000.0 + " segundos");
+
+        //Se cierra el documento
+        salida.close();
+    }
+
+    public void quitarEtiquetas(String link, int num) throws IOException {
+        FileWriter fileEscritura = new FileWriter("Limpios\\" + this.numeroAString(num) + ".txt");
+        FileReader fileLectura = new FileReader(link);
+        BufferedReader bufred = new BufferedReader(fileLectura);
+        StringBuilder temporal = new StringBuilder();
+        String archivoCompleto, linea;
+
+        while((linea = bufred.readLine())!= null)                               //Mientras no se haya llegado al final del archivo
+        {
+            String lineaPura = linea.replaceAll("<.*?>","");
+            temporal.append(lineaPura + "\n");
+        }
+
+        archivoCompleto = temporal.toString();
+
+        fileEscritura.write(archivoCompleto);
+
+        fileEscritura.close();
+        fileLectura.close();
+
+    }
+
     //Abre con el buscador predeterminado la página con el link que recibe de parametro
     public void abrirPagina(String link) throws IOException
     {
